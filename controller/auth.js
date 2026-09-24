feaea2d7d1132233d881a8c5190e3d95dbd0e0fd
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-09-24 14:25:07 +0500

    Creating jwt token when signing-up

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,19 @@
 const Users = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res)=>{
-    const newUser = await Users.create(req.body);
+    const newUser = await Users.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        passwordConfirm:req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({id:newUser._id}, process.env.SECRET_JWT_KEY, {expiresIn:process.env.JWT_EXPIRES_IN})
 
     res.status(201).json({
         status:"success",
+        token,
         data:newUser
     })
 }
